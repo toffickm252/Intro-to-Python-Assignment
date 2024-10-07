62823988a4110e2a904b91a23de14f0b49201f92
Toffick Mohammed <toffickm252@gmail.com>
2024-10-07 07:55:15 +0000

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 07:39:15 2024
+
+@author: Thinkpad
+"""
+
+name=input("What is your name ?")
+age=int(input("What is your age ?"))
+location=input("Where do you live ?")
+
+
+"Hello [name], you are [age] years old and live in [location]."
+
+print("Hello "+name+" you are "+str(age)+" years old and live in "+location)
